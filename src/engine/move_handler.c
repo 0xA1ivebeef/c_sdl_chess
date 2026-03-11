92e0506cfe3ca4b37d86b197fd56f24ee556bf5a
0xA1ivebeef <roemmling.jon@gmail.com>
2026-03-11 18:32:48 +0100

    full incremental zobrist hashing with full special move handling

--- src/engine/move_handler.c
+++ src/engine/move_handler.c
@@ -1,7 +1,7 @@
 
 #include "engine/move_handler.h"
 
-void handle_pawn_promotion(uint64_t* bb, Move* m)
+void handle_pawn_promotion(Position* pos, Move* m)
 {
     assert(m->flags >= KNIGHT_PROMOTION && m->flags <= QUEEN_PROMOTION);
 
@@ -10,32 +10,43 @@ void handle_pawn_promotion(uint64_t* bb, Move* m)
     promote_piece += pawn; // fixing color 
 
     // replace pawn with promoted piece
-    bb[pawn] &= ~(1ULL << m->dest);
-    bb[promote_piece] |= (1ULL << m->dest); 
+    pos->bb[pawn] &= ~(1ULL << m->dest);
+    pos->bb[promote_piece] |= (1ULL << m->dest); 
+
+    pos->zobrist_hash ^= zobrist_table[pawn * 64 + m->dest];
+    pos->zobrist_hash ^= zobrist_table[promote_piece * 64 + m->dest];
 }
 
-void handle_enpassant(int player, uint64_t* bb, Move* m)
+void handle_enpassant(Position* pos, Move* m)
 {
-    int capture_sq = player ? m->dest + 8 : m->dest - 8;
-    int captured_pawn = player ? BLACK_PAWN : WHITE_PAWN;
+    int capture_sq = pos->player ? m->dest + 8 : m->dest - 8;
+    int captured_pawn = pos->player ? BLACK_PAWN : WHITE_PAWN;
 
     assert(capture_sq >= 0 && capture_sq < 64);
     
-    bb[captured_pawn] &= ~(1ULL << capture_sq); // capture enpassanted pawn
+    pos->bb[captured_pawn] &= ~(1ULL << capture_sq); // capture enpassanted pawn
+
+    pos->zobrist_hash ^= zobrist_table[captured_pawn * 64 + capture_sq];
 }
 
-void handle_castling(uint64_t* bb, Move* m)
+void handle_castling(Position* pos, Move* m)
 {
     int rook = (m->start > 32) ? WHITE_ROOK : BLACK_ROOK; 
     if(m->start > m->dest)
     {
-        bb[rook] &= ~(1ULL << (m->dest - 2));
-        bb[rook] |= (1ULL << (m->dest + 1));
+        pos->bb[rook] &= ~(1ULL << (m->dest - 2));
+        pos->bb[rook] |= (1ULL << (m->dest + 1));
+
+        pos->zobrist_hash ^= zobrist_table[rook * 64 + m->dest - 2];
+        pos->zobrist_hash ^= zobrist_table[rook * 64 + m->dest + 1];
     }
     else
     {
-        bb[rook] &= ~(1ULL << (m->dest + 1));
-        bb[rook] |= (1ULL << (m->dest - 1));
+        pos->bb[rook] &= ~(1ULL << (m->dest + 1));
+        pos->bb[rook] |= (1ULL << (m->dest - 1));
+        
+        pos->zobrist_hash ^= zobrist_table[rook * 64 + m->dest + 1];
+        pos->zobrist_hash ^= zobrist_table[rook * 64 + m->dest - 1];
     }
 }
 
@@ -46,16 +57,16 @@ void handle_special_move(Position* pos, Move* m)
     switch (m->flags)
     {
         case CASTLE_FLAG:
-            handle_castling(pos->bb, m);
+            handle_castling(pos, m);
             break;
         case ENPASSANT_FLAG:
-            handle_enpassant(pos->player, pos->bb, m);
+            handle_enpassant(pos, m);
             break;
         case KNIGHT_PROMOTION:
         case BISHOP_PROMOTION:
         case ROOK_PROMOTION:
         case QUEEN_PROMOTION:
-            handle_pawn_promotion(pos->bb, m); 
+            handle_pawn_promotion(pos, m); 
             break;
         case DOUBLE_PAWN_PUSH:
             pos->enpassant = m->dest + 8*(pos->player ? 1 : -1);
@@ -148,6 +159,8 @@ void undo_move(Position* pos, Move* m, Undo* undo)
     pos->halfmove = undo->halfmove; 
     pos->fullmove = undo->fullmove;
 
+    pos->zobrist_hash = undo->zobrist_hash;
+
     pos->player ^= 1;
     update_occ(pos);
 }
@@ -165,6 +178,7 @@ void save_state(Position* pos, Move* m, Undo* undo)
     undo->zobrist_hash   = pos->zobrist_hash;
 }
 
+// TODO zobrist updating promotions, castling and enpassant captures
 void apply_move(Position* pos, Move* m, Undo* undo)
 {   
     save_state(pos, m, undo);
@@ -189,9 +203,24 @@ void apply_move(Position* pos, Move* m, Undo* undo)
 
     pos->zobrist_hash ^= zobrist_table[moved_piece * 64 + m->dest];
 
+    // remove old enpassant square do this before enpassant square is updated through this move
+    if (pos->enpassant != INVALID_SQUARE)
+        pos->zobrist_hash ^= zobrist_enpassant[pos->enpassant % 8];
+
+    // remove old castlerights
+    pos->zobrist_hash ^= zobrist_castling[pos->castle_rights];
+
     // special move handling after normal move
+    // updates enpassant square
     handle_special_move(pos, m); 
     update_castle_rights(pos, m); 
+
+    // add new castle rights
+    pos->zobrist_hash ^= zobrist_castling[pos->castle_rights];
+
+    // add new enpassant square
+    if (pos->enpassant != INVALID_SQUARE)
+        pos->zobrist_hash ^= zobrist_enpassant[pos->enpassant % 8];
 
     // move clocks
     int pawn_move = (moved_piece % WHITE_PAWN == 0);
